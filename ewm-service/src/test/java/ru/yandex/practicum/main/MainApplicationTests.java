9216d27297957ad995502c4c5f48912606be50d1
Sergey <e9orkin.s@yandex.ru>
2022-12-02 22:09:02 +0300

    fix: delete test

--- ewm-service/src/test/java/ru/yandex/practicum/main/MainApplicationTests.java
+++ ewm-service/src/test/java/ru/yandex/practicum/main/MainApplicationTests.java
@@ -1,13 +1,7 @@
 package ru.yandex.practicum.main;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
